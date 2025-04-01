79c8329807403e4e0799930928f206255d18f619
ngocnhan09022005 <nhantn0516@ut.edu.vn>
2025-04-01 21:26:00 +0700

    Add methods to FeedbackRepository for user, vaccine, and comment searches

--- src/main/java/edu/uth/childvaccinesystem/repositories/FeedbackRepository.java
+++ src/main/java/edu/uth/childvaccinesystem/repositories/FeedbackRepository.java
@@ -1,10 +1,24 @@
 package edu.uth.childvaccinesystem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.uth.childvaccinesystem.entities.Feedback;
 
+import java.util.List;
+
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+    List<Feedback> findByUserId(Long userId);
+
+    List<Feedback> findByVaccineId(Long vaccineId);
+
+    @Query("SELECT f FROM Feedback f WHERE LOWER(f.comments) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Feedback> findByCommentsContaining(@Param("keyword") String keyword);
+
+    @Query("SELECT f FROM Feedback f WHERE f.createdDate > :date")
+    List<Feedback> findByCreatedDateAfter(@Param("date") String date);
 }
