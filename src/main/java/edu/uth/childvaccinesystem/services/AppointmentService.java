9f34002dcca1cb26e37dca442dc52e8209437026
nguyenxuanhai-arch <hainx0952@ut.edu.vn>
2025-04-02 21:00:18 +0700

    Remove ChildVaccineScheduleTrackingSystem subproject and update AppointmentService to remove unimplemented methods

--- src/main/java/edu/uth/childvaccinesystem/services/AppointmentService.java
+++ src/main/java/edu/uth/childvaccinesystem/services/AppointmentService.java
@@ -5,12 +5,10 @@ import edu.uth.childvaccinesystem.entities.Appointment;
 public class AppointmentService {
 
     public Appointment bookAppointment(Appointment appointment) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'bookAppointment'");
     }
 
     public void cancelAppointment(Long id) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'cancelAppointment'");
     }
 
