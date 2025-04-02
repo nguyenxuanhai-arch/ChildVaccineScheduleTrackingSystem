3c070125a64e91a472ae1415cbf7a62191441062
ngocnhan09022005 <nhantn0516@ut.edu.vn>
2025-04-02 08:45:19 +0700

    Add query methods to DashboardService for enhanced data retrieval and management

--- src/main/java/edu/uth/childvaccinesystem/services/DashboardService.java
+++ src/main/java/edu/uth/childvaccinesystem/services/DashboardService.java
@@ -3,8 +3,11 @@ package edu.uth.childvaccinesystem.services;
 import edu.uth.childvaccinesystem.repositories.UserRepository;
 import edu.uth.childvaccinesystem.repositories.AppointmentRepository;
 import edu.uth.childvaccinesystem.repositories.VaccineRepository;
+import edu.uth.childvaccinesystem.repositories.ChildRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class DashboardService {
@@ -18,6 +21,9 @@ public class DashboardService {
     @Autowired
     private VaccineRepository vaccineRepository;
 
+    @Autowired
+    private ChildRepository childRepository;
+
     public long getUserCount() {
         return userRepository.count();
     }
@@ -28,5 +34,28 @@ public class DashboardService {
 
     public long getVaccineCount() {
         return vaccineRepository.count();
+    }
+
+    public long getChildCount() {
+        return childRepository.count();
+    }
+
+    public long getTodayAppointmentCount() {
+        LocalDate today = LocalDate.now();
+        return appointmentRepository.countByDate(today);
+    }
+
+    public long getAppointmentCountBetweenDates(LocalDate startDate, LocalDate endDate) {
+        return appointmentRepository.countByDateBetween(startDate, endDate);
+    }
+
+    public long getUsedVaccineCount() {
+        return vaccineRepository.countByUsedTrue();
+    }
+
+    public long getNewUsersThisMonth() {
+        LocalDate startOfMonth = LocalDate.now().withDayOfMonth(1);
+        LocalDate endOfMonth = LocalDate.now().withDayOfMonth(LocalDate.now().lengthOfMonth());
+        return userRepository.countByCreatedAtBetween(startOfMonth, endOfMonth);
     }
 }
